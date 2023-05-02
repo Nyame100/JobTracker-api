1e31d575bace71eb526cbc52b3644933d9352643
nyame100 <nyamerichmond100@gmail.com>
2023-05-02 08:49:18 +0200

    Add sort functionality and pagination
    The sort functionality enables a user to sort job searches. jobs
    are sorted based on [lastest, oldest, alphabetical order]
    The pagination restricts the number of returned jobs to 10 per page

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -12,9 +12,43 @@ const getAllJobs = async (req, res) => {
   if (search) {
     queryObject.position = { $regex: search, $options: "i" };
   }
+  // add stuff based on Condition
+
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
   let result = Job.find(queryObject);
+
+  // Sort Functionality
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
+  // Setup Pagination
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
